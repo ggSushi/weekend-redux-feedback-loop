3bfd0601f326cb17a5a2553be1c8c43008627388
Glonel Dimapilis <gdimapilis@aol.com>
2023-04-16 23:35:38 -0500

    set up Routes in App.jsx with paths for all components. Also prepared the components in the Form folder.

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import axios from 'axios';
 import './App.css';
+import {HashRouter as Router, Route, Link} from 'react-router-dom';
+import FeelingInput from '../Form/Feeling.jsx'
 
 function App() {
 
@@ -10,6 +12,26 @@ function App() {
         <h1 className='App-title'>Feedback!</h1>
         <h4>Don't forget it!</h4>
       </header>
+      <Router>
+        <Route exact path="/">
+          {/* Feeling component here*/}
+        </Route>
+        <Route exact path="/understanding-page-2">
+          {/* Understanding component here */}
+        </Route>
+        <Route exact path="/support-page-3">
+          {/* Support component here */}
+        </Route>
+        <Route exact path="/comment-step-4">
+          {/* Comment component here */}
+        </Route>
+        <Route exact path="/review-step-5">
+          {/* Review component here */}
+        </Route>
+        <Route exact path="/thank-you-final-step">
+          {/* ThankYou component here */}
+        </Route>
+      </Router>
     </div>
   );
 }
---
+++ src/components/Form/Feeling.jsx
@@ -0,0 +1,14 @@
+import {useSelector, useDispatch} from 'react-redux';
+
+function FeelingInput() {
+
+  return(
+    <>
+      <h3>How are you feeling?</h3>
+      <input />
+      <button >Next</button>
+    </>
+  )
+}
+
+export default FeelingInput;
